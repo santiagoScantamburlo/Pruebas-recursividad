95dcfaa07ee0e3eb31e8e63c6ce0bcc0e18fce1d
Pufu <santiago.scantamburlo@est.fi.uncoma.edu.ar>
2019-11-28 17:50:00 -0300

    D2811H1749

---
+++ src/javaapplication1/TestVector.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author Cefai
+ */
+public class TestVector {
+    public static void main(String[] args) {
+     Vector[] v; 
+      v=new Vector[10];
+   
+    
+    }
+   public static void menu() {
+        System.out.println("Ingrese una opcion a realizar");
+        System.out.println("1: Escalonar");
+        System.out.println("2: Calcular determinante");
+        System.out.println("3: Calcular coordenadas genericas");
+        System.out.println("4: Verificar departamento");
+        System.out.println("5: Ordenar las propiedades por precio");
+        System.out.println("6: Contar la cantidad de propiedades disponibles con una cantidad de ambientes determinada");
+        System.out.println("7: Encontrar la casa en alquiler con la mayor superficie");
+        System.out.println("8: Verificar la existencia de una casa con un precio dado");
+        System.out.println("9: Ordenar las propiedades por superficie");
+        System.out.println("0: Finalizar");
+    }
+    
+}
---
+++ src/javaapplication1/Vector.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author Cefai
+ */
+public class Vector {
+   
+    private Object [] vector;
+    
+ public Vector (Object [] vec){
+ vector=vec;
+ }
+}
